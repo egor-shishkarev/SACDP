a7150e6a1c9e87c6b7f528b81cba92bdef55e486
egor-shishkarev <egorsiskarev92@gmail.com>
2024-02-18 19:19:00 +0300

    Change "if, else" to "match" construction

--- Homework1/DegreeSeries/Program.fs
+++ Homework1/DegreeSeries/Program.fs
@@ -1,21 +1,24 @@
 ﻿let degreeList n m =
 
     let rec fastPow acc pow = 
-        if pow = 0 then 1
-        else if pow % 2 = 0 then fastPow (acc * acc) (pow / 2)
-        else acc * fastPow (acc * acc) ((pow - 1) / 2)
+        match pow with
+        | 0 -> 1
+        | a -> 
+            match a % 2 with 
+            | 0 ->  fastPow (acc * acc) (pow / 2)
+            | a -> acc * fastPow (acc * acc) ((pow - 1) / 2)
 
     let rec createList index list =
-        if index = m then list
-        else createList (index + 1) (List.head list * 2 :: list)
+        match index with 
+        | 0 -> list 
+        | a -> createList (index - 1) (List.head list * 2 :: list)
 
-    let lst = createList 0 [fastPow 2 n]
+    let rec reverseList acc list = 
+        match list with 
+        | [] -> acc
+        | hd :: tl -> reverseList (hd :: acc) (tl)  
 
-    let rec reverseList newList index = 
-        if index = List.length lst - 1 then newList
-        else reverseList (lst[index + 1] :: newList) (index + 1)
-
-    reverseList [] -1
+    [fastPow 2 n] |> createList m |> reverseList []
 
 let n = 2
 let m = 8
