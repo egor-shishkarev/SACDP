58e693ec934e9b83f807d50f08122f28e0fb7a12
egor-shishkarev <egorsiskarev92@gmail.com>
2024-02-18 19:57:39 +0300

    Change "if, else" to "match" construction

--- Homework1/SearchNumber/Program.fs
+++ Homework1/SearchNumber/Program.fs
@@ -1,9 +1,10 @@
 ﻿let searchNumber list x = 
-    let rec subFunction index = 
-        if index >= List.length list - 1 && list[index] <> x then - 1
-        else if list[index] = x then index
-        else subFunction (index + 1)
-    subFunction 0
+    let rec subFunction list index = 
+        match list with 
+        | hd :: tl when hd = x -> index
+        | [] -> -1
+        | hd :: tl -> subFunction (tl) (index + 1)
+    subFunction list 0
 
 let list = [1; 2; 3; 5]
 printfn "Index of 2 is %d, index of 4 is %d" (searchNumber list 2) (searchNumber list 4)
