76594089837ddf43b8afd4b279fa8557de6a2a9a
egor-shishkarev <egorsiskarev92@gmail.com>
2024-02-18 18:43:59 +0300

    Change "if, else" to "match" construction

--- Homework1/ListConversion/Program.fs
+++ Homework1/ListConversion/Program.fs
@@ -1,8 +1,9 @@
 ﻿let conversion lst = 
-    let rec subFunction newList index = 
-        if index = List.length lst - 1 then newList
-        else subFunction (lst[index + 1] :: newList) (index + 1)
-    subFunction [] -1
+    let rec subFunction lst res = 
+        match lst with 
+        | [] -> res
+        | hd :: tl -> subFunction (tl) (hd :: res)
+    subFunction lst []
 
 let list = [10; 9; 8; 7; 6; 5; 4; 3; 2; 1]
 printfn "Initial list is - %A, reversed list - %A" list (conversion list)
