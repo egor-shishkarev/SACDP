ff3b8dd8af147626264336e2a1c2f71dd679a319
egor-shishkarev <egorsiskarev92@gmail.com>
2024-03-15 22:20:01 +0300

    Add map for trees function

---
+++ Homework2/MapForTrees/MapForTrees.fs
@@ -0,0 +1,11 @@
+﻿namespace Trees
+
+module MapForTrees = 
+    type Tree<'a> =
+        | Tree of 'a * Tree<'a> * Tree<'a>
+        | Tip of 'a
+
+    let rec mapForTrees (tree: Tree<'a>) func =
+        match tree with 
+            | Tree (node, leftTree, rightTree) -> Tree (func node, mapForTrees leftTree func, mapForTrees rightTree func)
+            | Tip (node) -> Tip (func node)
