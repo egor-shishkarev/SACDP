33df2e7d6ab1c86fae46f4deda412e06f80688d9
egor-shishkarev <egorsiskarev92@gmail.com>
2024-04-11 16:52:59 +0300

    Change adjacency matrix to adjacency list, add object return using .Infect()

--- Homework5/LocalNetwork/Program.fs
+++ Homework5/LocalNetwork/Program.fs
@@ -14,49 +14,48 @@ type Computer (osType: OS) =
     member this.Infect() =
         if not infected then
             infected <- true
+        this
 
 
-type LocalNetwork (computers: Computer list, connections: bool list list, infectionProbability: Map<OS, float>) =
+type LocalNetwork (computers: Computer list, connections: int list list, infectionProbability: Map<OS, float>) =
     member this.Computers = computers
     member this.Connections = connections
     member this.InfectionProbability = infectionProbability
+    member this.IsFinal = false
 
     member this.Step() = 
         let mutable infectedOnThisStep = []
         let addInfected newInfected = infectedOnThisStep <- newInfected :: infectedOnThisStep
 
-        for i = 0 to computers.Length - 1 do
+        for i = 0 to connections.Length - 1 do
             if computers.[i].Infected then
-                for j = 0 to computers.Length - 1 do
-                    if connections.[i].[j] && not computers.[j].Infected then
-                        let probability = infectionProbability.[computers.[j].OS]
-                        if probability > 0.0 && probability >= Random().NextDouble() then
-                            addInfected computers.[j]
-        List.iter (fun (x: Computer) -> x.Infect()) infectedOnThisStep
+                for j in connections.[i] do
+                    let connectedComputer = computers.[j]
+                    if not connectedComputer.Infected then
+                            let probability = infectionProbability.[connectedComputer.OS]
+                            if probability > 0.0 && probability >= Random().NextDouble() then
+                                addInfected connectedComputer
+        
+        infectedOnThisStep <- List.map (fun (x: Computer) -> x.Infect()) infectedOnThisStep
 
     member this.PrintNetworkStatus() =
         for i = 0 to computers.Length - 1 do
             printfn "%d - %A" i computers.[i].Infected
 
-let computers = [Computer(OS.Windows); Computer(OS.Linux);
+let computers = [Computer(OS.Windows).Infect(); Computer(OS.Linux);
     Computer(OS.Windows); Computer(OS.Windows); Computer(OS.Linux);
     Computer(OS.MacOS)]
-computers.[0].Infect()
-let net = new LocalNetwork(computers, 
-    [[false; true; false; false; false; false]; 
-    [true; false; false; false; false; true]; 
-    [false; false; false; false; false; true];
-    [false; false; false; false; false; true]; 
-    [false; false; false; false; false; true]; 
-    [false; true; true; true; true; false]],
+let connections = [[1]; [0; 5]; [5]; [5]; [5]; [1; 2; 3; 4]]
+
+let net = new LocalNetwork(computers, connections,
     Map[OS.Linux, 1; OS.Windows, 1; OS.MacOS, 1])
 
-net.Step() |> ignore
+net.Step()
 net.PrintNetworkStatus()
 Console.WriteLine()
-net.Step() |> ignore
+net.Step()
 net.PrintNetworkStatus()
 Console.WriteLine()
-net.Step() |> ignore
+net.Step()
 net.PrintNetworkStatus()
  
