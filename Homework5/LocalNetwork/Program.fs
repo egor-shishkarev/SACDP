585e2449b7fe6990a2a7706ad7bd00af830ccb64
egor-shishkarev <egorsiskarev92@gmail.com>
2024-04-11 18:40:32 +0300

    Add splitting into connectivity components, check for the end of infection

--- Homework5/LocalNetwork/Program.fs
+++ Homework5/LocalNetwork/Program.fs
@@ -42,20 +42,53 @@ type LocalNetwork (computers: Computer list, connections: int list list, infecti
         for i = 0 to computers.Length - 1 do
             printfn "%d - %A" i computers.[i].Infected
 
+    member this.GetConnectedComponents() =
+        let visited = Array.create computers.Length false
+        let mutable components = []
+
+        let rec dfs node (comp: int list) =
+            visited.[node] <- true
+            let mutable comp = node :: comp
+            List.iter (fun neighbour ->
+                if not visited.[neighbour] then
+                    comp <- dfs neighbour comp
+            ) connections.[node]
+            comp
+
+        for i = 0 to connections.Length - 1 do
+            if not visited.[i] then
+                components <- dfs i [] :: components
+
+        components
+
+    member this.PerformInfect() =
+        printfn("Список смежности сети - \n%A") connections
+        printfn("Начальное состояние сети - ")
+        this.PrintNetworkStatus()
+
+        let components = this.GetConnectedComponents()
+        let isInfectedComponent = List.map (fun (x: int list) -> 
+            List.fold (fun acc i -> acc || computers.[i].Infected) false x) <| components
+        let isAnythingToInfect = List.map (fun (x: int list) -> 
+            List.fold (fun acc i -> acc || (not computers.[i].Infected && infectionProbability.[computers.[i].OS] > 0.0) && isInfectedComponent.[List.findIndex (fun i -> i = x) components]) false x)
+
+        let isFinal = List.fold (fun acc x -> acc || x) false << isAnythingToInfect
+
+        let mutable i = 1
+        while (isFinal components) <> false do
+            printfn("Шаг %d") i
+            this.Step()
+            this.PrintNetworkStatus()
+            i <- i + 1
+
+        printfn("Заражено максимально возможное количество компьютеров")
+
 let computers = [Computer(OS.Windows).Infect(); Computer(OS.Linux);
     Computer(OS.Windows); Computer(OS.Windows); Computer(OS.Linux);
-    Computer(OS.MacOS)]
-let connections = [[1]; [0; 5]; [5]; [5]; [5]; [1; 2; 3; 4]]
+    Computer(OS.MacOS).Infect(); Computer(OS.Windows); Computer(OS.Windows); Computer(OS.Windows);]
+let connections = [[1; 3]; [2; 0]; [1]; [0]; [5; 6]; [4]; [4; 7]; [6]; []]
 
 let net = new LocalNetwork(computers, connections,
     Map[OS.Linux, 1; OS.Windows, 1; OS.MacOS, 1])
 
-net.Step()
-net.PrintNetworkStatus()
-Console.WriteLine()
-net.Step()
-net.PrintNetworkStatus()
-Console.WriteLine()
-net.Step()
-net.PrintNetworkStatus()
- 
+net.PerformInfect()
